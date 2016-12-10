40ed728a2935ecbccdc92a41d73d706ed0e322e8
Boyan <boyan.barakov@gmail.com>
2016-12-10 15:46:39 +0100

    change port

--- server.js
+++ server.js
@@ -3,30 +3,15 @@ var express = require('express'),
     app = express(),
     pg = require('pg');
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT * FROM users;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-
-    });
-
-});
 
 app.post('/tropo', (req, res) => {
   console.log("POST request")
-  console.log("req", req);
   res.send("it works");
 });
 
 app.get('/tropo', (req, res) => {
   console.log("GET request")
-  console.log("req", req);
   res.status(200).send("it twerks").end();
 });
 
-app.listen(8080);
+app.listen(5000);
