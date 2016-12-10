abe40a1ee950b585c2bc39eec86b02f7543b7dbb
Boyan <boyan.barakov@gmail.com>
2016-12-10 15:28:42 +0100

    add db

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require('express'),
     querystring = require('querystring'),
-    app = express()
+    app = express(),
+    pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT * FROM users;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+
+    });
+
+});
 
 app.post('/tropo', (req, res) => {
   console.log("POST request")
@@ -11,7 +26,7 @@ app.post('/tropo', (req, res) => {
 app.get('/tropo', (req, res) => {
   console.log("GET request")
   console.log("req", req);
-  res.send("it twerks");
+  res.status(200).send("it twerks");
 });
 
 app.listen(8080);
