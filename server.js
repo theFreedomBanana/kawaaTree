2309f710f480182e0824aa48520186a749a54698
Boyan <boyan.barakov@gmail.com>
2016-12-10 16:14:17 +0100

    test db connect

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express'),
     querystring = require('querystring'),
     app = express(),
-    pg = require('pg');
+    pg = require('pg').Client();
 
+pg.connect(process.env.DATABASE_URL, err => {
+  if (err)
+    throw err;
+});
 
 app.post('/tropo', (req, res) => {
   console.log("POST request")
@@ -10,8 +14,11 @@ app.post('/tropo', (req, res) => {
 });
 
 app.get('/tropo', (req, res) => {
-  console.log("GET request")
-  res.status(200).send("it twerks").end();
+  console.log("GET request");
+  pg.query('SELECT * FROM users', (err, res) => {
+    res.json(res);
+  });
+  //res.status(200).send("it twerks").end();
 });
 
 app.listen(process.env.PORT || 5000);
