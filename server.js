6907325a149cc3fcb1284ca335cfdd3b3f73b18b
Boyan <boyan.barakov@gmail.com>
2016-12-11 10:06:54 +0100

    clear session

--- server.js
+++ server.js
@@ -21,8 +21,16 @@ function isNewUser(number) {
   return client.query("select * from users where phonenumber='" + number + "';").then(data => {
     if (data.rows.length == 0) {
       client.query(`insert into users(phonenumber) values (${number})`);
-      const session = create_session(number);
-      session['missing_name'] = true;
+      return true;
+    }
+    return false;
+  });
+}
+
+function getUsersForTree(id) {
+  return client.query("select phonenumber  from users where phonenumber='" + number + "';").then(data => {
+    if (data.rows.length == 0) {
+      client.query(`insert into users(phonenumber) values (${number})`);
       return true;
     }
     return false;
@@ -43,7 +51,7 @@ function create_session(phonenumber) {
 }
 
 function find_session(phonenumber) {
-  console.log(sessions)
+  //console.log(sessions)
   return sessions.find(s => s.phonenumber == phonenumber);
 }
 
@@ -57,31 +65,38 @@ app.post('/tropo', (req, res) => {
   const message = req.body.text;
   const num = req.body.phonenumber;
   const session = find_session(num);
-  console.log(session)
+  console.log(message);
     if (!session) {
-      console.log('newUser');
       create_session(num);
-      res.send(JSON.stringify({"question": "Bonjour, nous sommes content de vous compter parmi nous ! Pouvez-vous nous communiquer le numéro de l'arbre ?"})).end();
+      res.send(JSON.stringify({
+        "question": "Bonjour, nous sommes content de vous compter parmi nous ! Pouvez-vous nous communiquer le numéro de l'arbre ?"
+      })).end();
     }
     else if (session.tree_id_missing) {
-      console.log('askedTreeId');
+     // wit.message(message).then(data => {
+     //   console.log(data.entities);
+     // });
       session.tree_id_missing = false;
       session.tree_id = message;
-      res.send(JSON.stringify({"question": "Merci bcp ! Vous pouvez proposer un nom de cet arbre."})).end();
+      res.send(JSON.stringify({
+        "question": "Merci bcp ! Vous pouvez proposer un nom de cet arbre."
+      })).end();
     }
     else if (session.tree_name_missing) {
-      console.log('askedTreeName');
       session.tree_name_missing = false;
       session.tree_name = message;
-      res.send(JSON.stringify({"question": "Super ! Et en fait comment vous appelez-vous ?"})).end();
+      res.send(JSON.stringify({
+        "question": "Super ! Et en fait comment vous appelez-vous ?"
+      })).end();
     }
     else if (session.firstname_missing) {
-      console.log('askedName');
       session.firstname_missing = false;
-      const [ first_name, last_name ] = message.split(' ');
-      session.firstname = first_name;
-      res.send(JSON.stringify({"question": 'Génial ' + session.firstname  + ' ! Nous vous tiendrons au courant des prochaines rencontres et des actions liées à ' + session.tree_name + '. Bonne journée !'})).end();
-      const q = "INSERT INTO users (phone_number, first_name, tree_id) VALUES (" + session.phonenumber + ",'" + session.firstname + "'," + session.tree + ")";
+      //const [ first_name, last_name ] = message.split(' ');
+      session.firstname = message;
+      res.send(JSON.stringify({
+        "question": 'Génial ' + session.firstname  + ' ! Nous vous tiendrons au courant des prochaines rencontres et des actions liées à ' + session.tree_name + '. Bonne journée !'
+      })).end();
+      const q = "INSERT INTO users (phonenumber, firstname, tree_id) VALUES ('" + session.phonenumber + "','" + session.firstname + "','" + session.tree_id + "')";
       client.query(q);
     }
     else if (session.idle) {
@@ -89,16 +104,29 @@ app.post('/tropo', (req, res) => {
       res.send(JSON.stringify({"question": "Pour plus d'info vous contacter ..."})).end();
     }
     else {
+      session = { 
+        phonenumber: phonenumber, 
+        firstname_missing: false, 
+        lastname_missing: true, 
+        tree_id_missing: true, 
+        tree_name_missing: true,
+        idle: true
+      };
       res.send(JSON.stringify({"question": "Merci et à bientôt !"})).end();
     }
 });
 
-app.get('/tropo', (req, res) => {
+const {Wit, log} = require('node-wit');
+
+const wit = new Wit({
+  accessToken: process.env.WIT_TOKEN
+});
+
+app.get('/visit', (req, res) => {
   console.log("GET request")
   if (req.query.phone_number) {
 
   };
-  res.send(JSON.stringify("it twerks"));
   res.end();
 });
 
